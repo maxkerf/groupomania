ce40322860fe8c3f95bfb8b73eb418a46ea06c8c
Maxime Kerfourn <maxime.kerfourn@orange.fr>
2022-03-02 17:40:06 +0100

    frontend init

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+
+	next();
+});
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Welcome to Groupomania 🔥"));
+app.get("/", (req, res) =>
+	res.status(200).json({ message: "Welcome to Groupomania 🔥" })
+);
 
 module.exports = app;
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>{{ message }}</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			message: "",
+		};
+	},
+	async created() {
+		try {
+			const res = await fetch("http://localhost:3000");
+			const data = await res.json();
+
+			this.message = data.message;
+		} catch (err) {
+			console.error(err);
+		}
+	},
+};
+</script>
