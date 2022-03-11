f875b12e354ead01ca3da5a1420572b033727b4d
Maxime Kerfourn <maxime.kerfourn@orange.fr>
2022-03-11 11:58:37 +0100

    update not found vue

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -1,3 +1,22 @@
 <template>
-	<h1>Not Found</h1>
+	<div>
+		<router-link v-if="login.userId === -1" to="/">Home</router-link>
+		<h1>Not Found</h1>
+	</div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+	computed: {
+		...mapState(["login"]),
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+a {
+	display: inline-block;
+	margin-bottom: 1rem;
+}
+</style>
